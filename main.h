231f4fc116cf91f71399ff410fd84a0d85a6deee
psychohight <7298@holbertonstudents.com>
2024-03-15 01:29:17 +0100

    test

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*standart library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/*prototype of functions.c*/
+int print_c(va_list argc);
+int _putchar(char c);
+int print_percent(void);
+int print_str(va_list argc);
+int _printf(const char *format, ...);
+int print_int(va_list argc);
+void help(int n, int *count);
+/**
+*struct format - prototype of function
+*@id: description
+*@f: desc
+*/
+typedef struct format
+{
+char *id;
+int (*f)();
+} match;
+#endif
---
+++ printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *_printf - print string
+ * @format: the specifier character
+ * Return: nb of character printed.
+*/
+int _printf(const char *format, ...)
+{
+    match m[] = {
+    {"%c", print_c},
+    {"%s", print_str},
+    {"%%", print_percent},
+    {"%d", print_int},
+    {"%i", print_int}
+    };
+va_list argc;
+int i = 0, j, count = 0;
+va_start(argc, format);
+if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+return (-1);
+while (format[i] != '\0')
+{
+    for (j = 0; j < 5; j++)
+    {
+    if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+        {
+        count = count + m[j].f(argc);
+        i = i + 2;
+        }
+    }
+_putchar(format[i]);
+    i++;
+    count++;
+}
+va_end(argc);
+return (count);
+}
+
+
+
+
+
+
